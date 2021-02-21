b2f0962902e71fcd35dd5267be711e69bd8f491d
Randy <afantasticwatermelon@gmail.com>
2021-02-21 09:15:25 -0800

    Cleanup whitespace, updated strings to hopefully be more readable/maintainable, watch_message now links to watched message

--- hasibot.py
+++ hasibot.py
@@ -55,7 +55,7 @@ def day_print(guild_id, lst_users):
     ret_str = ""
 
     for n, user_id in enumerate(lst_users):
-        user = client.get_guild(int(guild_id)).get_member(int(user_id)) 
+        user = client.get_guild(int(guild_id)).get_member(int(user_id))
         if user.nick is not None:
             name = user.nick
         else:
@@ -106,7 +106,7 @@ async def on_ready():
             dict_guild = {'guild_id':str(guild.id), 'channel_id':'','message_id':''}
             df_guild = pd.DataFrame([dict_guild])
             df_all_guilds = pd.concat([df_all_guilds,df_guild])
-    
+
     DataConnector.run_query("TRUNCATE TABLE {}.guilds".format(SCHEMA_NAME))
     DataConnector.write_data(df_all_guilds, SCHEMA_NAME,'guilds', 'append')
     print("Bot is ready!")
@@ -123,7 +123,7 @@ async def on_guild_join(guild):
     '''
     # This query checks if an entry already exists before inserting into the table.
     # If the entry exists, nothing is done.
-    query = open("data/guild_join_query.txt").read().replace("\n"," ").format(SCHEMA_NAME, str(guild.id))
+    query = open("data/guild_join_query.txt").read().format(SCHEMA_NAME, str(guild.id))
     DataConnector.run_query(query)
 
 @client.event
@@ -138,8 +138,11 @@ async def on_guild_remove(guild):
     '''
     # This query removes the guild ID from the guilds table and removes all
     # entries with that same guild ID
-    DataConnector.run_query("DELETE FROM " + SCHEMA_NAME + ".guilds WHERE guild_id = '" + str(guild.id) + "';" + \
-                            "DELETE FROM " + SCHEMA_NAME + ".days WHERE guild_id = '" + str(guild.id) + "';")
+    DataConnector.run_query(("""DELETE FROM {0}.guilds
+                                WHERE guild_id = '{1}';
+                                DELETE FROM {0}.days
+                                WHERE guild_id = '{1}';
+                             """).format(SCHEMA_NAME, str(guild.id)))
 
 @client.event
 async def on_raw_reaction_add(payload):
@@ -153,14 +156,19 @@ async def on_raw_reaction_add(payload):
     Arguments:
     - payload   RawReactionActionEvent  a Discord object
     '''
-#TODO: improvements like calling the query after simple early return checks? 
+#TODO: improvements like calling the query after simple early return checks?
 #      is channel needed?
     global day_emotes
     guild_id = str(payload.guild_id)
-    df_channel = DataConnector.read_data("SELECT message_id,channel_id FROM " + SCHEMA_NAME + ".guilds WHERE guild_id = '" + guild_id + "'")
-    channel = client.get_channel(int(df_channel['channel_id'][0]))
 
+    # Run query to get the channel and message IDs for the requesting guild
+    df_channel = DataConnector.read_data(("""SELECT message_id, channel_id
+                                             FROM {0}.guilds
+                                             WHERE guild_id = '{1}'
+                                          """).format(SCHEMA_NAME, guild_id))
+    channel = client.get_channel(int(df_channel['channel_id'][0]))
     message_id = df_channel['message_id'][0]
+
     if (channel is None) or (message_id == ''):
         return
 
@@ -187,14 +195,19 @@ async def on_raw_reaction_remove(payload):
     Arguments:
     - payload   RawReactionActionEvent  a Discord object
     '''
-#TODO: improvements like calling the query after simple early return checks? 
+#TODO: improvements like calling the query after simple early return checks?
 #      is channel needed?
     global day_emotes
     guild_id = str(payload.guild_id)
-    df_channel = DataConnector.read_data("SELECT message_id,channel_id FROM " + SCHEMA_NAME + ".guilds WHERE guild_id = '" + guild_id + "'")
+
+    # Run query to get the channel and message IDs for the requesting guild
+    df_channel = DataConnector.read_data(("""SELECT message_id, channel_id
+                                             FROM {0}.guilds
+                                             WHERE guild_id = '{1}'
+                                          """).format(SCHEMA_NAME, guild_id))
     channel = client.get_channel(int(df_channel['channel_id'][0]))
- 
     message_id = df_channel['message_id'][0]
+
     if (channel is None) or (message_id == ''):
         return
 
@@ -204,7 +217,12 @@ async def on_raw_reaction_remove(payload):
         # user ID on that emote day from the days table
         user = str(payload.user_id)
         day = str(payload.emoji.name)
-        DataConnector.run_query("DELETE FROM " + SCHEMA_NAME + ".days WHERE user_id='" + user + "' AND day = '" + day + "'")
+
+        # Run another query to delete a user if they unreacted
+        DataConnector.run_query(("""DELETE FROM {0}.days
+                                    WHERE user_id = '{1}' AND 
+                                          day = '{2}'
+                                 """).format(SCHEMA_NAME, user, day))
 
 @client.event
 async def on_message(message):
@@ -246,8 +264,11 @@ async def on_message(message):
         if len(cmd) < 2:
             return
 
-        DataConnector.run_query("UPDATE " + SCHEMA_NAME + ".guilds SET channel_id = '" + \
-                                 cmd[1] + "' WHERE guild_id='" + guild_id + "'")
+        # Run query to update channel ID stored in guilds table for requesting guild
+        DataConnector.run_query(("""UPDATE {0}.guilds
+                                    SET channel_id = '{1}'
+                                    WHERE guild_id = '{2}'
+                                 """).format(SCHEMA_NAME, cmd[1], guild_id))
         await message.channel.send('Watching channel: ' + cmd[1])
 
     elif cmd[0] == '~.watch_message':
@@ -260,29 +281,35 @@ async def on_message(message):
         if len(cmd) < 2:
             return
 
-        DataConnector.run_query("UPDATE " + SCHEMA_NAME + ".guilds SET message_id = '" + \
-                                 cmd[1] + "' WHERE guild_id='" + guild_id + "';" + \
-                                 "DELETE FROM " + SCHEMA_NAME + ".days WHERE guild_id='" + guild_id + "';"+ \
-                                 "INSERT INTO " + SCHEMA_NAME + ".days VALUES ('" + guild_id + "', '', '', '')")
-        await message.channel.send('Watching message: https://discord.com/channels/' + \
-                                    guild_id                 + '/' + \
-                                    str(message.channel.id)  + '/' + \
-                                    cmd[1])
+        # Run query to:
+        # 1. Updates the message ID stored in guilds table for requesting guild
+        # 2. Deletes any stored entries in days table for the requesting guild
+        # 3. Inserts dummy entry into days table for requesting guild (for print_info)
+        DataConnector.run_query(("""UPDATE {0}.guilds
+                                    SET message_id = '{1}'
+                                    WHERE guild_id = '{2}';
+                                    DELETE FROM {0}.days
+                                    WHERE guild_id = '{2}';
+                                    INSERT INTO {0}.days
+                                    VALUES ('{2}', '', '', '')
+                                 """).format(SCHEMA_NAME, cmd[1], guild_id))
+        str_final = 'Watching message: https://discord.com/channels/{}/{}/{}'
+        await message.channel.send(str_final.format(guild_id, str(message.channel.id), cmd[1]))
 
 #     elif '~.update_ut' in message.content.lower():
 #         lst_members = client.get_guild(guild_id).members
-#         
+#
 #         df_ut = pd.DataFrame()
 #         for member in lst_members:
 #             name =  member.nick
 #             if name is None:
 #                 name = member.name
-#         
+#
 #             dict_data = {'user_id': str(member.id), 'nick':str(name).lower()}
 #             df_data = pd.DataFrame([dict_data])
 #             df_ut = pd.concat([df_ut,df_data])
 #         df_ut['guild_id'] = str(guild_id)
-#         
+#
 #         cols = ['guild_id','user_id','nick']
 #         df_ut = df_ut[cols]
 #         DataConnector.run_query("DELETE FROM hasibot_dev.user_table WHERE guild_id = '" + str(guild_id) + "'")
@@ -292,26 +319,40 @@ async def on_message(message):
     elif cmd[0] == '~.hasi':
         # Print out the specific day or all days
         # - cmd[1] optional day
-        df = DataConnector.read_data("SELECT * FROM " + SCHEMA_NAME + ".days WHERE guild_id = '" + guild_id + "'")
+        # Run query to get the user_id + day from the days table for the requesting guild
+        df = DataConnector.read_data(("""SELECT day, user_id
+                                         FROM {0}.days
+                                         WHERE guild_id = '{1}'
+                                      """).format(SCHEMA_NAME, guild_id))
 
-        # Print the specific day if exists
+        # Print the specific day if exists or all days
         if len(cmd) == 2 and cmd[1] in day_emotes:
-            str_final = "```md\n#" + cmd[1].capitalize() + ":\n{}\n```".format(day_print(guild_id,grab_day(df, cmd[1])))
+            str_final = ("```md\n"    + \
+                         "#{0}:\n{1}\n" + \
+                         "```").format(cmd[1].capitalize(),
+                                       day_print(guild_id, grab_day(df, cmd[1])))
         else:
-            str_final = "```md\n#Sun:\n{}\n#Mon:\n{}\n#Tue:\n{}\n#Wed:\n{}\n#Thu:\n{}\n#Fri:\n{}\n#Sat:\n{}\n```".format(
-                                                               day_print(guild_id, grab_day(df, 'sun')),
-                                                               day_print(guild_id, grab_day(df, 'mon')),
-                                                               day_print(guild_id, grab_day(df, 'tue')),
-                                                               day_print(guild_id, grab_day(df, 'wed')),
-                                                               day_print(guild_id, grab_day(df, 'thu')),
-                                                               day_print(guild_id, grab_day(df, 'fri')),
-                                                               day_print(guild_id, grab_day(df, 'sat')))
+            str_final = ("```md\n"     + \
+                         "#Sun:\n{0}\n" + \
+                         "#Mon:\n{1}\n" + \
+                         "#Tue:\n{2}\n" + \
+                         "#Wed:\n{3}\n" + \
+                         "#Thu:\n{4}\n" + \
+                         "#Fri:\n{5}\n" + \
+                         "#Sat:\n{6}\n" + \
+                         "```").format(day_print(guild_id, grab_day(df, 'sun')),
+                                       day_print(guild_id, grab_day(df, 'mon')),
+                                       day_print(guild_id, grab_day(df, 'tue')),
+                                       day_print(guild_id, grab_day(df, 'wed')),
+                                       day_print(guild_id, grab_day(df, 'thu')),
+                                       day_print(guild_id, grab_day(df, 'fri')),
+                                       day_print(guild_id, grab_day(df, 'sat')))
         await message.channel.send(str_final)
 
     elif cmd[0] == '~.roll_echo':
         # Roll a g30 echostone at normal rates (adv Sidhe)
         attempts, total_stat = get_echo_30()
- 
+
         resp = "It took {} attempts! The total stat is: {}".format(attempts, total_stat)
 
         await message.channel.send(resp)
@@ -321,14 +362,14 @@ async def on_message(message):
         attempts, total_stat = get_boosted_echo_30()
 
         resp = "It took {} attempts! The total stat is: {}".format(attempts, total_stat)
-        
+
         await message.channel.send(resp)
 
     elif cmd[0] == '~.print_info':
         # Prints some metadata and day counts
         # - cmd[1] optional gid flag
         # - cmd[2] if gid flag supplied, this is the guild ID as a string
-        query = open('data/day_query.txt').read().replace('\n',' ').format(SCHEMA_NAME)
+        query = open('data/day_query.txt').read().format(SCHEMA_NAME)
         df = DataConnector.read_data(query)
 
         # Add guild names for all found guilds
@@ -337,7 +378,7 @@ async def on_message(message):
             dict_map[str(i.id)] = i.name
 
         # Add in guild name to the DataFrame
-        df['guild_name'] = df['guild_id'].map(dict_map) 
+        df['guild_name'] = df['guild_id'].map(dict_map)
         cols = ['guild_name','guild_id','channel_id','message_id','sun','mon','tue','wed','thu','fri','sat']
         df = df[cols]
 
@@ -348,7 +389,7 @@ async def on_message(message):
         str_final = "```\n" + str(df.transpose()) + "\n```"
         await message.channel.send(str_final)
 
-    elif cmd[0] == '~.search': 
+    elif cmd[0] == '~.search':
         # Run Jerry search
         # - cmd[1]    type of search
         #   > item, items
@@ -376,7 +417,7 @@ async def on_message(message):
             if response.json()['data']['items'] == []:
                 await message.channel.send(content='No result\n' + the_link2)
                 return
- 
+
             item_id = response.json()['data']['items'][0]['id']
 
             the_link = "https://mabibase.com/item/" + str(item_id)
