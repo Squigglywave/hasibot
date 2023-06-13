de8c86178b07911590e696463c8b0335f0940d70
Jeremiah Liou <jeremiahjliou@gmail.com>
2023-06-12 18:49:09 -0700

    Create entrypoint

---
+++ run.sh
@@ -0,0 +1,3 @@
+export DATABASE_URL=postgresql+psycopg2://${PG_USER}:${PG_PASS}@${PG_HOST}/${PG_DB}                                                            │  hasibot
+                                                                                                                                               │squiggly@server1:~/Docker/hasibot$
+/usr/bin/python3 hasibot.py
